940d8ba83091f42577d4df1d22476ddefa08e2a5
berstearns <bernardo.stearns@gmail.com>
2024-11-01 10:24:34 +0000

    cli component of the project

--- alignments-cli/alignments_cli/__init__.py
+++ alignments-cli/alignments_cli/__init__.py
@@ -1,1 +1,22 @@
+from wasmer import engine, Store, Module, Instance
 from . import align
+
+store = Store()
+
+# Let's compile the module to be able to execute it!
+module = Module(store, """
+(module
+  (type (func (param i32 i32) (result i32)))
+  (func (export "sum") (type 0) (param i32) (param i32) (result i32)
+    local.get 0
+    local.get 1
+    i32.add))
+""")
+
+# Now the module is compiled, we can instantiate it.
+instance = Instance(module)
+
+# Call the exported `sum` function.
+result = instance.exports.sum(5, 37)
+
+print(result) # 42!
